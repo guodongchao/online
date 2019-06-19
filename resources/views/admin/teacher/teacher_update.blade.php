44a9375847181d1d26c745f32b6338058ac2599d
weiqi <1602337069@qq.com>
2019-06-20 07:44:46 +0800

    dsad

--- resources/views/admin/teacher/teacher_update.blade.php
+++ resources/views/admin/teacher/teacher_update.blade.php
@@ -33,29 +33,30 @@
             </div>
             <div class="baBody">
                 <div class="bbD">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师名称：<input type="text" name="teacher_name" class="input3" />
+                    <input type="hidden" name="teacher_id" value="{{$data['teacher_id']}}">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师名称：<input type="text" name="teacher_name" class="input3" value="{{$data['teacher_name']}}" />
                 </div>
                 <div class="bbD">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师涉及能力：<input type="text" name="teacher_type" class="input3" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师涉及能力：<input type="text" name="teacher_type" class="input3" value="{{$data['teacher_type']}}" />
                 </div>
                 <div class="bbD">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师授课风格：<input type="text" name="teacher_style" class="input3" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师授课风格：<input type="text" name="teacher_style" class="input3" value="{{$data['teacher_style']}}" />
                 </div>
                 <div class="bbD">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师头像：
                     <div class="bbDd">&nbsp;&nbsp;
-                            <div class="bbDImg">+</div>
-                            <input type="hidden" class="uplo" value="">
-                            <input type="file" class="file" id="file" name="file" />
-                        </div>
+                        <div class="bbDImg"><img src="{{$data['teacher_img']}}" width="300px" height="200px" alt=""></div>
+                        <input type="hidden" class="uplo" value="{{$data['teacher_img']}}">
+                        <input type="file" class="file" id="file" name="file" />
                     </div>
+                </div>
                 <div class="bbD">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;讲师简介：
                     <div class="row cl">
                         <label class="form-label col-xs-4 col-sm-2"></label>
                         <div class="layui-form-item">
                             <div class="layui-input-block"style="width: 73%;margin-left: 8%">
-                                <textarea id="LAY_demo1" style="display: none;" name="file"></textarea>
+                                <textarea id="LAY_demo1" style="display: none;" name="file">{{$data['teacher_desc']}}</textarea>
                             </div>
                         </div>
                     </div>
@@ -113,35 +114,27 @@
 
 
         $('#btn').click(function(){
+            var teacher_id=$("[name='teacher_id']").val()
             var teacher_name = $("[name='teacher_name']").val();
             var teacher_type = $("[name='teacher_type']").val();
             var teacher_style = $("[name='teacher_style']").val();
             var uplo = $(".uplo").val();
             var teacher_desc=layedit.getContent(news_contents);
             var data={};
+            data.teacher_id=teacher_id;
             data.teacher_name=teacher_name;
             data.teacher_type=teacher_type;
             data.teacher_style=teacher_style;
             data.teacher_img=uplo;
             data.teacher_desc=teacher_desc;
             $.post(
-                'teacher_do',
+                'teacher_update_do',
                 {data:data},
                 function(res){
                     if(res.code==0) {
-                        layer.open({
-                            type:0,
-                            content:'添加成功',
-                            btn:['继续添加','列表展示'],
-                            yes:function(index,layero){
-                                location.href="teacher";
-                                return true;
-                            },
-                            btn2:function(){
-                                location.href="teacher_list";
-                                return true;
-                            }
-                        })
+                        layer.msg(res.msg,{time:3000},function () {
+                            window.location.href="teacher_list";
+                        });
                     }else if(res.code==1){
                         layer.msg(res.msg);
                     }
