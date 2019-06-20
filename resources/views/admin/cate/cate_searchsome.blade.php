f810c23cb02729ba40645a6ddc4a72b09f9ca64a
duan1001 <893889509@qq.com>
2019-06-20 10:55:10 +0800

    s

---
+++ resources/views/admin/cate/cate_searchsome.blade.php
@@ -0,0 +1,11 @@
+@foreach($data as $val)
+<tr>
+    <td width="66px" class="tdColor tdC ">{{$val['c_cate_id']}}</td>
+    <td width="355px" class="tdColor">{{$val['c_cate_name']}}</td>
+    <td width="355px" class="tdColor">{{date("Y-m-d H:i:s",$val['c_create_time'])}}</td>
+   <td width="355px" class="tdColor"><span class="cli" val="{{$val['c_cate_id']}}">{{$val['c_cate_sort']}}</span></td>
+    <td><a href="cate_update?cate_id={{$val['c_cate_id']}}"><img class="operation" src="img/update.png"></a>
+        <img class="operation delban" src="img/delete.png" onclick="cate_del($(this),{{$val['c_cate_id']}})">
+        </td>
+    </tr>
+@endforeach
