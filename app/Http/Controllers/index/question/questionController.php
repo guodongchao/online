11388173116051f1bdf9ad520c857dc51c080d29
root <root@localhost.localdomain>
2019-06-25 04:02:50 +0000

    a

--- app/Http/Controllers/index/question/questionController.php
+++ app/Http/Controllers/index/question/questionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\models\admin\CourseCate;
 use App\models\question;
+use App\models\records;
 use Illuminate\Support\Facades\Redis;
 class questionController extends Controller
 {
@@ -24,6 +25,9 @@ class questionController extends Controller
             $q_id=Redis::get($keys.$u_id.$c_id);
             if($q_id==null){
                 Redis::set($keys.$u_id.$c_id,$q_id);
+            }
+            if($q_id==''){
+                $q_id=0;
             }
         }else{
             $qq_id=Redis::get($keys.$u_id.$c_id,$q_id);
@@ -77,12 +81,16 @@ class questionController extends Controller
         $u_id=1;
         $c_id=$request->input('c_id');
         $q_id=$request->input('q_id');
+
         $key='examination';
         $keys='examinations';
         if($q_id==0){
             $q_id=Redis::get($keys.$u_id.$c_id);
             if($q_id==null){
                 Redis::set($keys.$u_id.$c_id,$q_id);
+            }
+            if($q_id==''){
+                $q_id=0;
             }
         }else{
             $qq_id=Redis::get($keys.$u_id.$c_id,$q_id);
@@ -93,17 +101,16 @@ class questionController extends Controller
         if($q_id==0.1){
             $q_id=0;
         }
-        $info1=Redis::zrange($key.$u_id.$c_id,0,0);
+        $info1=Redis::zrange($key.$u_id.$c_id,$q_id,$q_id);
         if($info1==false){
             $arr=question::where(['q_class'=>$c_id])->get()->toArray();
             $nums=count($arr);
             for($i=0;$i<10;$i++){
                 $val=array_rand($arr);
-                var_dump($val);
-                var_dump($nums);exit;
-                $val=$arr[$val];
-                $val=json_encode($val);
-                Redis::zadd($key.$u_id.$c_id,$i+1,$val);
+                $vall=$arr[$val];
+                unset($arr[$val]);
+                $vall=json_encode($vall);
+                Redis::zadd($key.$u_id.$c_id,$i+1,$vall);
             }
             $info1=Redis::zrange($key.$u_id.$c_id,$q_id,$q_id);
             foreach($info1 as $k=>$v){
@@ -116,12 +123,12 @@ class questionController extends Controller
             foreach($info1 as $k=>$v){
                 $data[]=json_decode($v);
             }
+           // var_dump($data);exit;
             foreach($data as $k=>$v){
                 $v->q_answer=explode(',',$v->q_answer);
             }
         }
         $num=Redis::ZCARD($key.$u_id.$c_id);
-
         $info=[
             'data'=>$data,
             'num' =>$num,
@@ -131,9 +138,93 @@ class questionController extends Controller
 
         return $info;
     }
+    public function question23(Request $request){
+        $u_id=1;
+        $c_id=$request->input('c_id');
+        $q_id=$request->input('q_id');
+        $q_id=$q_id-1;
+        $radio=$request->input('radio');
+        $key='examination';
+        $keys='examinations';
+        $info1=Redis::zrange($key.$u_id.$c_id,$q_id,$q_id);
+
+
+        foreach($info1 as $k=>$v){
+            $data[]=json_decode($v);
+        }
+        foreach($data as $kk=>$vv){
+            $vv->t_result=$radio;
+            $val=json_encode($vv);
+        }
+
+        //echo $key.$u_id.$c_id;
+        //echo $q_id;
+       // exit;
+        Redis::ZREMRANGEBYRANK($key.$u_id.$c_id,$q_id,$q_id);
+        Redis::zadd($key.$u_id.$c_id,$q_id+1,$val);
+    }
+    public function question24(Request $request){
+        $u_id=1;
+        $key='examination';
+        $records='records';
+        $type=$request->input('type');
+        $c_id=$request->input('c_id');
+        if($type==1){
+        $score=$request->input('score');
+        if($score>=90){
+            $r_assess="优秀";
+        }
+        if($score>=60&&$score<90){
+            $r_assess="及格";
+        }
+        if($score<60){
+            $r_assess="不及格";
+        }
+        $c_cate_name=CourseCate::where(['c_cate_id'=>$c_id])->value('c_cate_name');
+        $where=[
+          'u_id'    =>$u_id,
+          'r_grade' =>$score,
+          'r_time'  =>time(),
+          'r_assess'=>$r_assess,
+          'c_cate_name'=>$c_cate_name
+        ];
+        $res=records::insert($where);
+
+
+        $info1=Redis::zrange($key.$u_id.$c_id,0,-1);
+        foreach($info1 as $k=>$v){
+           $arr[]=json_decode($v);
+        }
+        $arr=json_encode($arr);
+        $res=Redis::zadd($records.$u_id,$c_id,$arr);
+        if($res){
+            Redis::del($key.$u_id.$c_id);
+        }
+        }
+        if($type==2){
+            Redis::del($key.$u_id.$c_id);
+        }
+    }
     public function question3(Request $request){
-        $arr=CourseCate::get();
+        $arr=records::where(['is_status'=>1])->paginate(5);
         return view("index.question.question3",['arr'=>$arr]);
+    }
+    public function question33(Request $request){
+        $r_id=$request->input('r_id');
+        $res=records::where(['r_id'=>$r_id])->update(['is_status'=>2]);
+        if($res){
+            $info=[
+                'error'=>0,
+                'msg'  =>"OK"
+            ];
+        }else{
+            $info=[
+                'error'=>5000,
+                'msg'  =>"NO"
+            ];
+        }
+        return $info;
+
     }
     public function question4(Request $request){
         $arr=CourseCate::get();
--- resources/views/index/question/question3.blade.php
+++ resources/views/index/question/question3.blade.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="css/member.css"/>
 <script src="js/jquery-1.8.0.min.js"></script>
 <link rel="stylesheet" href="css/tab.css" media="screen">
+<link rel="stylesheet" type="text/css" href="/css/css.css" />
 <script src="js/jquery.tabs.js"></script>
 <script src="js/mine.js"></script>
 <script type="text/javascript">
@@ -59,120 +60,34 @@ $(function(){
 
 
 	<div class="membcont">
-<h3 class="mem-h3">我的课程</h3>
-<div class="box demo2" style="width:820px;">
-			<ul class="tab_menu" style="margin-left:30px;">
-				<li class="current">学习中</li>
-				<li>已学完</li>
-				<li>收藏</li>
-			</ul>
-			<div class="tab_box">
-				<div>
-					<ul class="memb_course">
-                    	
-                        <li>
-                            <div class="courseli">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg"></a>
-                            <p class="memb_courname"><a href="video" target="main" class="blacklink">会计基础</a></p>
-                            <div class="mpp">
-                                <div class="lv" style="width:20%;"></div>
-                            </div>
-                            <p class="goon"><a href="video" target="main"><span>继续学习</span></a></p>
-                            </div>
-                        </li>
-<li>
-                            <div class="courseli">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg"></a>
-                            <p class="memb_courname"><a href="video" target="main" class="blacklink">会计基础</a></p>
-                            <div class="mpp">
-                                <div class="lv" style="width:20%;"></div>
-                            </div>
-                            <p class="goon"><a href="video" target="main"><span>继续学习</span></a></p>
-                            </div>
-                        </li>
-<li>
-                            <div class="courseli">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg"></a>
-                            <p class="memb_courname"><a href="video" target="main" class="blacklink">会计基础</a></p>
-                            <div class="mpp">
-                                <div class="lv" style="width:20%;"></div>
-                            </div>
-                            <p class="goon"><a href="video" target="main"><span>继续学习</span></a></p>
-                            </div>
-                        </li>
-                        
-                        
-						
-                        
-                       
-                        <div style="height:10px;" class="clearfix"></div>
-                    </ul>
-                    
-				</div>
-				<div class="hide">
-					<div>
-					<ul class="memb_course">
-                    	
-                        <li>
-                            <div class="courseli">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg"></a>
-                            <p class="memb_courname"><a href="coursecont" target="main" class="blacklink">会计基础</a></p>
-							<div class="mpp">
-                                <div class="lv" style="width:100%;"></div>
-                            </div>
-                            <p class="goon"><a href="coursecont" target="main"><span>查看课程</span></a></p>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="courseli">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg"></a>
-                            <p class="memb_courname"><a href="coursecont" target="main" class="blacklink">会计基础</a></p>
-							<div class="mpp">
-                                <div class="lv" style="width:100%;"></div>
-                            </div>
-                            <p class="goon"><a href="coursecont" target="main"><span>查看课程</span></a></p>
-                            </div>
-                        </li>
-                        
-                       
-                        <div class="clearfix" style="height:10px;"></div>
-                    </ul>
-                    
-				</div>
-				</div>
-				<div class="hide">
-					<div>
-					<ul class="memb_course">                   	
-                        <li>
-                            <div class="courseli mysc">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg" class="mm"></a>
-                            <p class="memb_courname"><a href="video" target="main" class="blacklink">会计基础</a></p>
-                            <div class="mpp">
-                                <div class="lv" style="width:20%;"></div>
-                            </div>
-                            <p class="goon"><a href="#"><span>继续学习</span></a></p>
-							<div class="mask"><span class="qxsc"  title="移除收藏">▬</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="courseli mysc">
-                            <a href="video" target="main" target="_blank"><img width="230" src="images/c8.jpg" class="mm"></a>
-                            <p class="memb_courname"><a href="video" target="main" class="blacklink">会计基础</a></p>
-                            <div class="mpp">
-                                <div class="lv" style="width:20%;"></div>
-                            </div>
-                            <p class="goon"><a href="#"><span>继续学习</span></a></p>
-							<div class="mask"><span class="qxsc"  title="移除收藏">▬</span></div>
-                            </div>
-                        </li>                                     
-                        <div class="clearfix" style="height:10px;"></div>
-                    </ul>
-				</div>
-				</div>
-				
-			</div>
-		</div>
-</div>
+<h3 class="mem-h3">我的记录</h3>
+
+
+    <div class="conShow">
+        <table border="1" cellspacing="0" cellpadding="0">
+            <tr>
+                <td width="200px" class="tdColor tdC">ID</td>
+                <td width="200px" class="tdColor">分类名称</td>
+                <td width="200px" class="tdColor">得分</td>
+                <td width="200px" class="tdColor">评估</td>
+                <td width="200px" class="tdColor">考试时间</td>
+                <td width="200px" class="tdColor">操作</td>
+            </tr>
+            @foreach($arr as $k=>$v)
+                <tr>
+                    <td width="200px" class="tdColor tdC">{{$v['r_id']}}</td>
+                    <td width="200px" class="tdColor">{{$v['c_cate_name']}}</td>
+                    <td width="200px" class="tdColor">{{$v['r_grade']}}</td>
+                    <td width="200px" class="tdColor">{{$v['r_assess']}}</td>
+                    <td width="200px" class="tdColor">{{ date('Y-m-d H:i:s',$v['r_time']) }}</td>
+                    <td width="200px" class="tdColor"> <p class="goon"><a href="javascript:;" onclick="del({{$v['r_id']}})" target="main"><span>删除</span></a></p></td>
+                </tr>
+            @endforeach
+        </table>
+        <div class="pull-right">{{$arr->links()}}</div>
+    </div>
+
+    </div>
 
 
 <div class="clearh"></div>
@@ -218,3 +133,20 @@ $(function(){
 </body>
 
 <!-- InstanceEnd --></html>
+<script>
+    function del(r_id){
+        var data={
+            r_id:r_id
+        }
+        $.ajax({
+            type: 'post',
+            data: data,
+            url: '/index/question33',
+            dataType: 'json',
+            success: function (msg) {
+                alert(msg.msg)
+                window.location.reload();
+            }
+        })
+    }
+</script>
