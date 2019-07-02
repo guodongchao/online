11ecccf865e76d74eda9c0a2342145c37ec72fa3
duan1001 <893889509@qq.com>
2019-07-02 10:00:34 +0800

    s

--- app/Http/Controllers/index/login/loginController.php
+++ app/Http/Controllers/index/login/loginController.php
@@ -219,7 +219,7 @@ class loginController extends Controller
         $u_pwd = $request->input('u_pwd');
         $openid = $request->input('openid');
         $result = user::where("u_openid",$openid)->first();
-        if($result){
+        if(count($result)){
             $resopnse = [
                 'code' => 101,
                 'msg' => '抱歉！该微信账号已绑定其他账号'
