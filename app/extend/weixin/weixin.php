e95289c8bbd565cf00fefe4756680a393d869878
weiqi <1602337069@qq.com>
2019-07-04 09:37:10 +0800

    dasd

---
+++ app/extend/weixin/weixin.php
@@ -0,0 +1,43 @@
+<?php
+class weixin{
+    public function sendGet($str){
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL, $str);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+        $info = curl_exec($ch);
+        curl_close($ch);
+        return $info;
+    }
+
+    public function sendPost($url,$arr){
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SAFE_UPLOAD,true);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        @curl_setopt($ch, CURLOPT_POSTFIELDS, $arr);
+        $info = curl_exec($ch);
+        curl_close($ch);
+        return $info;
+    }
+
+    public function arr2Xml( $arr ){
+        $xml = '<xml version="1.0" encoding="UTF-8"> ';
+        foreach(  $arr as $key => $value ){
+            if (is_numeric($value)){
+                $xml.="<".$key.">".$value."</".$key.">";
+            }else{
+                $xml.="<".$key."><![CDATA[".$value."]]></".$key.">";
+            }
+
+        }
+        $xml .= '</xml>';
+        return $xml;
+    }
+}
